d7dbab004937ffc6f89be7a076671a349aced4d5
uripro1 <urisntk@gmail.com>
2015-03-13 17:42:44 +0100

    -- TODO POR EL MOMENTO

--- js/cookieManager.js
+++ js/cookieManager.js
@@ -1,13 +1,23 @@
-function eraseCookie(c_name) {
-    document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-}
-
 function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires="+d.toUTCString();
-    document.cookie = cname + "=" + cvalue + "; " + expires;
-}
+            var d = new Date();
+            d.setTime(d.getTime() + (exdays*24*60*60*1000));
+            var expires = "expires="+d.toUTCString();
+            document.cookie = cname + "=" + cvalue + "; " + expires;
+        }
+
+function setLoginCookie() {
+        console.log("error");
+            var uvalue = document.forms["login"]["email"].value;
+            var pvalue = document.forms["login"]["password"].value;
+    
+            var d = new Date();
+            var exdays = 5;
+            d.setTime(d.getTime() + (exdays*24*60*60*1000));
+            var expires = "expires="+d.toUTCString();
+            console.error("User: " + uvalue + " - Pass: " + pvalue);
+            document.cookie = "user =" + uvalue + "; " + expires;
+            document.cookie = "pass =" + pvalue + "; " + expires;
+        }
 
 function getCookie(cname) {
     var name = cname + "=";
@@ -15,7 +25,13 @@ function getCookie(cname) {
     for(var i=0; i<ca.length; i++) {
         var c = ca[i];
         while (c.charAt(0)==' ') c = c.substring(1);
-        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
     }
     return "";
+}
+
+function checkCookie(cname) {
+    var user = getCookie("user");
+    if (user != "") {
+    }
 }
